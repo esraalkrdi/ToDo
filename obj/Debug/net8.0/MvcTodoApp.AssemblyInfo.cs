c6666f55838af9857e62ef2b8c4a464fcdae81fb
esraalkrdi <esraalkrdi@gmail.com>
2025-05-31 01:57:45 +0500

    Added (EditTask) feature

--- obj/Debug/net8.0/MvcTodoApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcTodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e4e54b6632a6fc2b366c50b6b51164ce6c732ac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcTodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
